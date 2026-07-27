f1b50518290ab2cd91ede1cb14955836a5b1fc71
remote <1711351315@qq.com>
2026-07-27 15:08:29 +0000

    course0学习

--- code/course0/offline/offline_infer.py
+++ code/course0/offline/offline_infer.py
@@ -1,23 +1,27 @@
 import os
 
-os.environ["VLLM_USE_V1"] = "1"  # 必须在 import vllm 之前！
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+os.environ["VLLM_USE_V1"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 from vllm import LLM, SamplingParams
 
-prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "Write a poem about China:",
-    "Who won the world series in 2020?",
-]
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+def main():
+    prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "Write a poem about China:",
+        "Who won the world series in 2020?",
+    ]
+    sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
-llm = LLM(model="Qwen/Qwen3-1.7B", enforce_eager=True)
+    llm = LLM(model="/root/model/Qwen3-0.6B", enforce_eager=True)
 
-outputs = llm.generate(prompts, sampling_params)
+    outputs = llm.generate(prompts, sampling_params)
 
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+if __name__ == "__main__":
+    main()
